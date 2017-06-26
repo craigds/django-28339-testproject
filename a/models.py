2164bbf5e2c6907f7a224ba3669eb0fb681d062b
Craig de Stigter <craig.destigter@koordinates.com>
2017-06-26 12:38:33 +1200

    Turns out it's not just ForeignKeys.
    Added a char and int field, same thing

--- a/models.py
+++ a/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 
 class B(models.Model):
-    pass
+    charfield = models.CharField(max_length=1, default='', db_index=False)
+    intfield = models.IntegerField(default=0, db_index=False)
 
 
 class C(models.Model):
